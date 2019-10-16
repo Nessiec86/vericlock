11850dd4a3768f567ccaa781d7515d6627f67a3b
tecnologias01 <tecnologias01@iMac-de-tecnologias01.local>
2019-10-16 16:20:27 +0200

    if conditions on text input

--- src/pages/ClockSign.js
+++ src/pages/ClockSign.js
@@ -22,7 +22,7 @@ class Clocksign extends Component {
     }
     
     handleSubmit = (event) => {
-        
+        if (this.state.text.length === 9) {
         user.read (event)
             .then((data) => { 
                 if (data.data.id > 0) {
@@ -37,7 +37,9 @@ class Clocksign extends Component {
                     isLoading: false
                 });
             });
-        
+        } else {
+
+        }
     };
     
     handleWork = (event) => {
@@ -77,7 +79,6 @@ class Clocksign extends Component {
     };
     
     
-    
     handleSubmit2 = () => {
         user.tokenGet(undefined)
         
@@ -87,9 +88,18 @@ class Clocksign extends Component {
     handleChange = event => {
         const { name, value } = event.target;
         this.setState({ [name]: value });
+        if (value.length < 9){
+            this.setState({
+                data: {
+                    data: {
+                        id:'',
+                        name: '',
+                        start: '',
+                    },
+                },
+            })
+        }
     };
-
-   
 
     render() {
         let time = 0
@@ -98,44 +108,45 @@ class Clocksign extends Component {
         const dateObj = new Date(start * 1000); 
         const utcString = dateObj.toString(); 
         time = utcString.slice(16, 24); 
-        
+        console.log(this.state.data.data)
+
         return (
-                <div className='clock-sign'>
-                    <input
-                        placeholder="DNI"
-                        onChange={this.handleChange}
-                        name="text"
-                        value={text}
-                        style={{margin:'0 0 1rem 0'}}
-                    />
-                    <p style={{margin:'1rem 0'}}>{name}</p>
-                    {start > 0 ?
-                        <p>Hora de Entrada: {time}</p>
+            <div className='clock-sign'>
+                <input
+                    placeholder="DNI"
+                    onChange={this.handleChange}
+                    name="text"
+                    value={text}
+                    style={{margin:'0 0 1rem 0'}}
+                />
+                <p style={{margin:'1rem 0'}}>{name}</p>
+                {start > 0 ?
+                    <p>Hora de Entrada: {time}</p>
+                :
+                    <p style={{margin:'1.3rem 0'}}></p>
+                }
+                <Button variant="success"className='val' onClick={() => this.handleSubmit(text)}>VALIDAR</Button>
+                <div style={{position: 'absolute', alignSelf: 'center', margin: '1rem 0'}}>
+                    { verify === true ?
+                        <i className="far fa-thumbs-up" style={{fontSize: '5rem', color: '#ac451d'}}></i>
                     :
-                        <p style={{margin:'1.3rem 0'}}></p>
-                    }
-                    <Button variant="success"className='val' onClick={() => this.handleSubmit(text)}>VALIDAR</Button>
-                    <div style={{position: 'absolute', alignSelf: 'center', margin: '1rem 0'}}>
-                        { verify === true ?
-                            <i className="far fa-thumbs-up" style={{fontSize: '5rem', color: '#ac451d'}}></i>
-                            :
-                            <div>
-                            </div>
-                        }
-                    </div>
-                    { name.length < 9  ?
                         <div>
-                        </div>
-                        :
-                        <div style={{display:'flex', justifyContent:'center'}}>
-                            {start === 0 ?
-                                <Button variant="success" className='work' onClick={() => this.handleWork(text)}>EMPEZAR A TRABAJAR</Button>
-                            :
-                                <Button variant="danger" className='work' onClick={() => this.handleWork(text)}>DEJAR DE TRABAJAR</Button>
-                            }
                         </div>
                     }
                 </div>
+                { name === '' || text.length < 9 ?
+                    <div>
+                    </div>
+                    : 
+                    <div style={{display:'flex', justifyContent:'center'}}>
+                        {start === 0 ?
+                            <Button variant="success" className='work' onClick={() => this.handleWork(text)}>EMPEZAR A TRABAJAR</Button>
+                        :
+                            <Button variant="danger" className='work' onClick={() => this.handleWork(text)}>DEJAR DE TRABAJAR</Button>
+                        }
+                    </div>
+                }
+            </div>
         );
     }
 };
